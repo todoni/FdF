ba714dfaa7fc26e8893bb8be52d987d1f3fb69b4
sohan <sohan@student.42seoul.kr>
2022-03-08 20:07:35 +0900

    [feat] ft_abs.c

--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,7 @@ int				ft_isdigit(int c);
 int				ft_isprint(int c);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+int				ft_abs(int x);
 int				ft_atoi(const char *str);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strchr(const char *s, int c);
